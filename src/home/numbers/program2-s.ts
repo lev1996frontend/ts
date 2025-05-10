48e5ef64b0fa5e375555dae322c38b5b713a0319
Lev Snegov <lev.snegov@bk.ru>
2025-05-10 23:32:31 +0300

    2025-05-10 23:32:31

--- src/home/numbers/program2-s.ts
+++ src/home/numbers/program2-s.ts
@@ -1,7 +1,12 @@
 import { run } from './run-signals'
 
-run('program2.txt', (value, send, error) => {
+let n1: number | undefined
+let operator: number | undefined
 
+run('program2.txt', (value, send, error) => {
+  if (value === 0) {
+    throw error
+  }
   const operators: Record <number, (a: number, b: number) => number>  = {
     1: (a, b) => a + b,
     2: (a, b) => a - b,
@@ -13,11 +18,17 @@ run('program2.txt', (value, send, error) => {
       return a / b
     },
   }
+  
+  if (typeof n1 === 'undefined') {
+    n1 = value
+    return
+  }
+  if (typeof operator === 'undefined') {
+    operator = value
+    return
+  }
 
-  const n1 = nextValue()
-  const operator = nextValue()
-  const n2 = nextValue()
-
+  const n2 = value
   const manipulation = operators[operator]
 
   if (!manipulation) {
@@ -27,8 +38,12 @@ run('program2.txt', (value, send, error) => {
   const result = manipulation(n1,n2)
 
   send(result)
+  n1 = undefined
+  operator = undefined
+ }
 
-})
+
+)
 
 /*
 Калькулятор
